9aebbb3f768de500b954433831d2f85cd7d01f76
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-05-12 19:23:07 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student(name, grade)
+        roster[grade] ||= [] #name ma byc array, jak to zmienic z stringa?
+        roster[grade] << name
+    end
+
+    def grade(n)
+        roster.select do |grade, name|
+           if grade == n
+            return name
+           end
+        end
+    end
+    def sort
+        students = {}
+        roster.each do |grade, name|
+            students[grade] = name.sort
+        end
+        students
+    end
+end
