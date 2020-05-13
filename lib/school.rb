2015970368cef9ea912009dde9a6608eb5d8b640
callas4ever <thetseringnorbu@gmail.com>
2020-05-13 01:19:13 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+class School
+attr_accessor :roster
+    def initialize(school_name)
+        @school_name=school_name
+        @roster={}
+    end
+
+
+
+    def add_student(name, grad)
+       if @roster[grad]==nil
+        @roster[grad]=[name]
+       else
+        @roster[grad].push(name)
+       end
+    end
+    def grade(grad)
+        @roster[grad]
+    end
+    def sort
+       @roster.sort{ |key, value|
+       @roster[key]=value.sort
+       }
+    end
+end
+
