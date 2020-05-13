3b1df8d7886133dbad93787607612aaeb28adf3b
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-12 22:54:31 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
+require 'pry'
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name, roster = {})
+    @roster = roster
+  end
+
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+    self.roster = @roster
+  end
+
+  def grade(grade)
+    result = self.roster.find{ |key, value|
+      grade == key
+    }.last
+    result
+  end
+
+  def sort
+    sorted_array = self.roster
+      .sort_by { |grade, students| grade }
+      .map { |student_class| [student_class[0], student_class[1].sort] }
+      .to_h
+  end
+end
