a1683c07b40f23b4a9bc40f934fa236d9cc85dd6
emotionalproof <chaserabenn@gmail.com>
2020-05-12 23:20:50 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+
+
+class School
+   def initialize(school)
+      @school = school
+      @roster = Hash.new{|h,k| h[k] = []}
+   end
+
+   def roster
+      @roster
+   end
+
+
+   def add_student(student, grade)
+      @roster[grade] << student
+   end
+
+   def grade(grade_number)
+      @roster[grade_number]
+   end
+
+   def sort
+      @roster.each { |k, v|  @roster[k].sort!}
+      @roster.sort.to_h
+   end
+end
